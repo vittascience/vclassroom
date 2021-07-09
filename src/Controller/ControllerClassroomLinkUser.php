c4bc508393e382821bee45a6f26e885046e94570
nas-be <naserbechata@gmail.com>
2021-07-09 11:53:29 +0200

    removed limitations for non admins and premiums

--- src/Controller/ControllerClassroomLinkUser.php
+++ src/Controller/ControllerClassroomLinkUser.php
@@ -57,7 +57,9 @@ class ControllerClassroomLinkUser extends Controller
                 $isAllowed = $learnerNumberCheck["isAdmin"] || $learnerNumberCheck["isPremium"];
 
                 // if not admin or premium
-                if(!$isAllowed ){
+                ///////////////////////////////////
+                // remove the limitations for CABRI
+                /* if(!$isAllowed ){
                     // get the number of students to add and compute the sum with the number of student already registered
                     $addedLearnerNumber = count($data['users']);
                     $totalLearnerCount = $learnerNumberCheck["learnerNumber"] + $addedLearnerNumber;
@@ -67,8 +69,9 @@ class ControllerClassroomLinkUser extends Controller
                         return ["isUsersAdded"=>false, "currentLearnerCount"=>$learnerNumberCheck["learnerNumber"], "addedLearnerNumber"=>$addedLearnerNumber];
                     }
                 }
-               
-                
+                */
+                // end remove the limitations for CABRI
+                /////////////////////////////////////////
                 /**
                  * End of learner number limiting
                  */
