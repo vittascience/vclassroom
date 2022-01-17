8f8e79ffbbe84c316193d905b3825eade335539c
Arouz <48770524+Arouz@users.noreply.github.com>
2022-01-17 17:40:27 +0100

    bad id ²

--- src/Controller/ControllerGroupAdmin.php
+++ src/Controller/ControllerGroupAdmin.php
@@ -818,7 +818,7 @@ class ControllerGroupAdmin extends Controller
                                 'group' => $application->getGroup(),
                                 'application' => $application->getApplication()
                             ]);
-                        $appActivitiesLimit = $this->entityManager->getRepository(ActivityRestrictions::class)->findOneBy(['application_id' => $application->getApplication()]);
+                        $appActivitiesLimit = $this->entityManager->getRepository(ActivityRestrictions::class)->findOneBy(['application' => $application->getApplication()]);
                         if ($appActivitiesLimit) {
                             $activityType = $appDetails->getActivityType();
                             $activityLimit = $appDetails->getMaxPerTeachers();
