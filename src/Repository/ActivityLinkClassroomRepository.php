40771644b53e446f9e7e061f28452b56fa683f97
ultimecreation <naserbechata@gmail.com>
2022-03-10 16:05:26 +0100

    add retro attributed activities to students for add_users method

--- src/Repository/ActivityLinkClassroomRepository.php
+++ src/Repository/ActivityLinkClassroomRepository.php
@@ -9,4 +9,20 @@ use Learn\Entity\Activity;
 
 class ActivityLinkClassroomRepository extends EntityRepository
 {
+    public function getRetroAttributedActivitiesByClassroom($classroom){
+        
+        $retroAttributedActivities = $this->getEntityManager()
+            ->createQueryBuilder()
+            ->select('alc')
+            ->from(ActivityLinkClassroom::class,'alc')
+            ->where("alc.classroom = :classroom AND alc.dateEnd >= :dateTrigger ")
+            ->setParameters(array(
+                'classroom' => $classroom,
+                'dateTrigger' => new \DateTime('now')
+            ))
+            ->getQuery()
+            ->getResult();
+
+        return $retroAttributedActivities;
+    }
 }
