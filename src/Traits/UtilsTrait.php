fd63cf256b2f96b4eb03e5167260884205bbd377
Arouz <cointe.remi@gmail.com>
2023-04-06 14:44:52 +0200

    Update UtilsTrait.php

--- src/Traits/UtilsTrait.php
+++ src/Traits/UtilsTrait.php
@@ -44,14 +44,14 @@ trait UtilsTrait {
             $demoStudent = str_replace('"', '', $demoStudent);
         }
 
-        $random = substr(str_shuffle(str_repeat($x='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', ceil(4/strlen($x)) )),1,4);
+        $random = substr(str_shuffle(str_repeat($x='0123456789', ceil(4/strlen($x)) )),1,4);
 
         $demoStudentToUpdate = $this->entityManager->getRepository(ClassroomLinkUser::class)->getAllDemoStudent($demoStudent);
         if ($demoStudentToUpdate) {
             foreach ($demoStudentToUpdate as $value) {
                 if (strlen($value->getPassword()) === 4) {
-                    $value->setPseudo("Élève $random");
-                    $value->setSurname("Élève$random");
+                    $value->setPseudo("Eleve-$random");
+                    $value->setSurname("Eleve-$random");
                     $this->entityManager->persist($value);
                     $this->entityManager->flush();
                 }
